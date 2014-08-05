3316d306686ce52f9ef110520472d7ef1a367501
Dimitar Bonev <dsbonev@gmail.com>
2014-08-05 09:30:01 +0300

    Open songs on youtube in new window

--- ki.informatik.uni-wuerzburg.de.js
+++ ki.informatik.uni-wuerzburg.de.js
@@ -1,3 +1,4 @@
 Array.prototype.slice.call(document.querySelectorAll('tr td:nth-child(2)')).forEach(function (item) {
   if (item.textContent.trim().toLowerCase() != 'new') item.parentElement.style.display = 'none';
+  else window.open("https://www.youtube.com/results?search_query=" + encodeURIComponent(item.parentElement.lastElementChild.textContent.trim() + ' ' + item.parentElement.lastElementChild.previousElementSibling.textContent.trim()), '_blank')
 })
